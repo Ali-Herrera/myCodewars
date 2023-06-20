cbf9bc56e151c8821ee73ec6a282f9146751ef73
Ali H <alisonherrera02@gmail.com>
2023-06-19 21:47:58 -0400

    adding more codewars challenges

---
+++ 8kyu/sum-arrays.js
@@ -0,0 +1,10 @@
+//DESCRIPTION:
+//Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+
+
+// My solution 
+function sum (numbers) {
+    let sumNums = numbers.reduce((acc, c) => acc + c, 0);
+     return sumNums
+ };
