1a9c825b698d6f775722e56d7384afa79a31a2f8
Ali H <alisonherrera02@gmail.com>
2023-08-05 14:33:26 -0400

    array codewars

---
+++ 8kyu/csvRepOfArray.js
@@ -0,0 +1,8 @@
+//Instructions
+//Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+
+//Solution 
+function toCsvText(array) {
+    return array.join('\n')
+ }
