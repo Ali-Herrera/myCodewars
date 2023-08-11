f5561efad5772e8ec1000926a0225c53c4bdd7c1
Ali H <alisonherrera02@gmail.com>
2023-08-10 20:47:05 -0400

    Even or odds codewars

---
+++ 8kyu/evenOrOdds.js
@@ -0,0 +1,10 @@
+
+//Instructions
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+//Solution 
+
+const evenOrOdds = num => { return num % 2 === 0 ? "Even" : "Odd" }
+
+
