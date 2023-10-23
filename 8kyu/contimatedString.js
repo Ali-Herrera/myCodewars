8ade23e731957288d92a68a25c39a41cd87d9347
Ali H <alisonherrera02@gmail.com>
2023-10-22 20:55:30 -0400

    string kata

---
+++ 8kyu/contimatedString.js
@@ -0,0 +1,34 @@
+//instructions
+
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+
+
+//Solution 
+
+const contamination = (text, char) => {
+    if (char === '' || text === ''){
+      return ''
+    }else {
+      return char.repeat(text.length)
+    }
+   }
+
+
+   //could also just do 
+
+function contamination(text, char){
+    return char.repeat(text.length)
+  }
+
